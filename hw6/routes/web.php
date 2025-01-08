d409379c2d0bea3bf328d58b6da5068ff44c2948
artem <shurtem@mail.ru>
2025-01-08 16:40:33 +0300

    Дз 6 сделано.

---
+++ hw6/routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Http\Controllers\BookController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('bookForm');
+});
+
+Route::get('/index', [BookController::class,'index'])->name('index');
+Route::post('/store', [BookController::class,'store']);
